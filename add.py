c342d941e3886b2a746f3e1e3bd264a0b58e0096
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-03-15 10:23:21 -0700

    1.2

--- add.py
+++ add.py
@@ -3,3 +3,19 @@ b = int(input("Enter Second Number"))
 
 c = a + b
 print("The result is ", c)
+
+recipe_1 = {'name': 'Tea', 'ingredients': [], 'cooking time': []}
+recipe_2 = {'name': 'Oatmeal', 'ingredients': [], 'cooking time': []}
+recipe_3 = {'name': 'Rice', 'ingredients': [], 'cooking time': []}
+recipe_4 = {'name': 'Cereal', 'ingredients': [], 'cooking time': []}
+recipe_5 = {'name': 'Omelette', 'ingredients': [], 'cooking time': []}
+
+
+recipe_2['ingredients'].append({'name': 'water'})
+recipe_2['ingredients'].append({'name': 'salt'})
+
+recipe_5['ingredients'].append({'name': 'eggs'})
+recipe_5['ingredients'].append({'name': 'peppers'})
+recipe_5['ingredients'].append({'name': 'cheese'})
+
+recipe_5['cooking time'].append({'amount':'5 minutes'})
